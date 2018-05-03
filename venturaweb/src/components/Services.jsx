e6af67ac7d06d604e0e0c1ddad5a97ffa78d0e53
vibe95 <vibi9505@gmail.com>
2018-05-03 16:48:48 -0400

    fix added pages

---
+++ venturaweb/src/components/Services.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Services.css';
+
+export default class Services extends Component{
+  render(){
+    return{
+      <div>
+        Services
+      </div>
+    }
+  }
+}
---
+++ venturaweb/src/components/Showcase.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Showcase.css';
+
+export default class Showcase extends Component{
+  render(){
+    return{
+      <div>
+        Showcase
+      </div>
+    }
+  }
+}
